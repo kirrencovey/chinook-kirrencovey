7eca5adc8a995bfaa88eaf6dc0e576a839d96f45
Kirren Covey <44656455+kirrencovey@users.noreply.github.com>
2019-05-20 09:44:49 -0500

    Update 24_top_2013_tracks.sql

--- sql-queries/24_top_2013_tracks.sql
+++ sql-queries/24_top_2013_tracks.sql
@@ -1,14 +1,11 @@
 -- Provide a query that shows the most purchased track of 2013.
 
-SELECT MAX(Highest.TrackName) TrackName, MAX(TrackSales) TrackSales
-FROM (
-	SELECT t.Name TrackName, COUNT(il.TrackId) TrackSales
+SELECT TOP 1 t.Name TrackName, COUNT(il.TrackId) TrackSales
 	FROM InvoiceLine il
 	JOIN Invoice i ON i.InvoiceId = il.InvoiceId
 	JOIN Track t ON t.TrackId = il.TrackId
 	WHERE DATEPART(yyyy, i.InvoiceDate) = 2013
 	GROUP BY t.Name
-) AS Highest
-;
+	ORDER BY TrackSales DESC
 
 -- Many tracks have the "highest" sales of 2, this just returns one of the many
