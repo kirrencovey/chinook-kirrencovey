0e1f10459645d92aacfa04efcac8d97631c07a99
Kirren Covey <44656455+kirrencovey@users.noreply.github.com>
2019-05-20 13:45:45 -0500

    Update 25_top_5_tracks.sql

--- sql-queries/25_top_5_tracks.sql
+++ sql-queries/25_top_5_tracks.sql
@@ -2,7 +2,6 @@
 
 SELECT TOP 5 t.Name AS TrackName, COUNT(il.Quantity) TrackSales
 FROM InvoiceLine il
-JOIN Invoice i ON il.InvoiceId = i.InvoiceId
 JOIN Track t ON il.TrackId = t.TrackId
 GROUP BY t.Name
 ORDER BY TrackSales DESC;
